0e3bc17331769d237b182dfa40f025aaaac4ec88
Holly Wilkerson <Hollyfaces1@gmail.com>
2016-03-14 21:43:31 -0600

    Initial setup for files for homework regarding bob etc. [#115467365]

--- app/controllers/current_controller.rb
+++ app/controllers/current_controller.rb
@@ -1,6 +1,30 @@
 class CurrentController < ApplicationController
   def time
   end
+
   def now
   end
+
+  def introduce
+  end
+
+  def bob
+  end
+
+  def and
+
+  end
+
+  def ann
+
+  end
+
+  def ted
+
+  end
+
+  def alice
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get 'time'=> 'current#time'
   get 'time/:now' => 'current#now'
+  get 'introduce' => 'current#introduce'
+  get 'introduce/:Bob' => 'current#bob'
+  get 'introduce/:Bob/:and' => 'current#and'
+  get 'introduce/:Bob/:and/:Ann' => 'current#ann'
+  get 'introduce/:Ted' => 'current#ted'
 end
